9a92b9a9f2a7230a9167155b2fe2cec0f55c8de0
Odavinh <penxerscky@gmail.com>
2020-04-16 17:27:26 +0300

    add loading

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './loading.scss';
+
+export default () => {
+  return (
+    <div className="spinner">
+      <div className="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,3 +1,4 @@
 export { default as MovieCard } from './MovieCard/MovieCard';
 export { default as AuthForm } from './AuthForm';
 export { default as Header } from './Header';
+export { default as Loading } from './Loading/Loading';
