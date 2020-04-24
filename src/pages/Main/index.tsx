4daffb8845f6ae1380d7f94135fd09372b47dba1
Odavinh <penxerscky@gmail.com>
2020-04-24 14:03:56 +0300

    add Terms of use

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -32,6 +32,41 @@ export default () => {
             <ChevronRight style={{ fontSize: '80px' }} />
           </div>
         </div>
+        <div className="body-text">
+          <div id="main-text">
+            <h1>Terms of use</h1>
+            <p>
+              1. All used audiovisual materials, links to which are posted on the website,
+              are the property of their manufacturer (owner of rights) and are protected
+              by international legal conventions.
+            </p>
+            <p>
+              2. You can use these materials only if the use is made for educational
+              purposes. These materials are intended for informational purposes only - for
+              other purposes you must buy a licensed entry.
+            </p>
+            <p>
+              3. All links to resources on the Web that contain videos and other
+              intellectual property are posted for free by visitors of the resource for
+              advertising or for informational purposes. Any information on the site is
+              automatically posted without any control from any side, which corresponds to
+              the generally accepted world practice of posting information on the
+              Internet.
+            </p>
+            <p>
+              4. The subject of the commercial aspect of this resource is the maintenance
+              and development of infrastructure that allows you to most comfortably view
+              demo audiovisual products, regardless of what is being watched. The video
+              materials presented on the resource are not the subject of financial
+              relations between users and site owners.
+            </p>
+            <p>
+              5. Using this resource, the user agrees that he is 18 years old and assumes
+              the risks associated with the possible harm caused by the use of information
+              in published materials.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
