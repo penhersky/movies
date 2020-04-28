1c10bf526124d752603037ba0c9d3fdf6849e4e5
Odavinh <penxerscky@gmail.com>
2020-04-28 15:44:00 +0300

    change page 404

--- src/modules/404/index.tsx
+++ src/modules/404/index.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Button from '@material-ui/core/Button';
+
+import './index.scss';
 
 export default () => {
-  return <div className="error-page">404</div>;
+  document.title = '404 Error';
+  return (
+    <div className="error-page">
+      <h1>404</h1>
+      <p>Page not found!</p>
+      <Button variant="contained" color="primary">
+        <NavLink to="/" className="link-404">
+          Go main
+        </NavLink>
+      </Button>
+    </div>
+  );
 };
