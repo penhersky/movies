4e6fedbf451ba528bf2e1d328f8bc1daf530f5fa
Odavinh <penxerscky@gmail.com>
2020-05-23 15:32:26 +0300

    move pagination from local state to global store

--- src/pages/Top/Top.tsx
+++ src/pages/Top/Top.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import { Pagination, MovieList } from '../../components';
@@ -8,18 +8,19 @@ import { topMovie } from '../../utils/createUrl';
 import { initialState } from '../../utils/api';
 import { useLazyFetch } from '../../hooks';
 
-import { SET_TOP100_MOVIES } from '../../types/movie';
+import { SET_TOP100_MOVIES, SET_ACTIVE_TOP100_PAGE } from '../../types/movie';
 
 import img from '../../image/top.jpg';
 
 import { Page } from '../../types/props';
 export default (props: Page) => {
-  const [page, setPage] = useState(1);
   document.title = `Space movies | TOP | ${props.activePage}`;
 
   const dispatch = useDispatch();
 
-  const { topMovies } = useSelector((state: any) => state.movieReducer);
+  const { topMovies, activeTopPage } = useSelector(
+    (state: any) => state.topMovieReducer,
+  );
   const [fetchData, data, loading, error] = useLazyFetch(initialState);
 
   useEffect(() => {
@@ -28,7 +29,7 @@ export default (props: Page) => {
   }, [data.results, dispatch]);
 
   const getPage = (page: number) => {
-    setPage(page);
+    dispatch({ type: SET_ACTIVE_TOP100_PAGE, activeTopPage: page });
     fetchData(topMovie(page), 'GET');
   };
 
@@ -38,7 +39,11 @@ export default (props: Page) => {
       <Parallax title='top 100 movies' img={img} opacity={0.6} />
       <div className='content'>
         <MovieList movies={topMovies} error={props.error || error} />
-        <Pagination activePage={page} countPage={5} newPage={getPage} />
+        <Pagination
+          activePage={activeTopPage}
+          countPage={5}
+          newPage={getPage}
+        />
       </div>
     </>
   );
